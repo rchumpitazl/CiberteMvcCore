cdd40966bc0fc97bae9f37efa6964aec8e1f3fb4
NetWeb-MJ <NetWeb-MJ@S000CVI00>
2017-06-22 22:22:24 -0500

    Selenium Automation

---
+++ Cibertec.E2E/Cibertec.Automation.Tests/BasicTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Cibertec.Automation.Tests
+{
+    public class BasicTest
+    {
+        [Fact]
+        public void Navigate_To_Google()
+        {
+            SimpleTest.Navigate();
+        }
+    }
+}
---
+++ Cibertec.E2E/Cibertec.Automation/SimpleTest.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace Cibertec.Automation
+{
+    public static class SimpleTest
+    {
+
+        public static void Navigate()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5000");
+            driver.Close();
+        }
+    }
+}
