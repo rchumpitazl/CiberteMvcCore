9b307f943743e036c2c653185ab9393138517925
NetWeb-MJ <NetWeb-MJ@S000CVI00>
2017-06-27 22:19:25 -0500

    Api de Customer con Gzip Compression

---
+++ Cibertec.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using Cibertec.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cibertec.WebApi.Controllers
+{
+    [Route("customer")]
+    public class CustomerController: Controller
+    {
+        private readonly IUnitOfWork _unit;
+        public CustomerController(IUnitOfWork unit)
+        {
+            _unit = unit;
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return Ok(_unit.Customers.GetAll());
+        }
+    }
+}
