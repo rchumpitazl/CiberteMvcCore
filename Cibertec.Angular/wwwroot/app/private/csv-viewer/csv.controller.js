43f83283844847e774728fe1c9b775da99fd11d2
NetWeb-MJ <NetWeb-MJ@S000CVI00>
2017-08-01 22:24:11 -0500

    webworker

--- Cibertec.Angular/wwwroot/app/private/csv-viewer/csv.controller.js
+++ Cibertec.Angular/wwwroot/app/private/csv-viewer/csv.controller.js
@@ -8,6 +8,8 @@
 
     function loginController(configService, $state) {
         var vm = this;
+        vm.orders = [];
+        vm.processFile = processFile;
 
         var fileInput = document.getElementById('csvViewer');
 
@@ -15,21 +17,41 @@
 
         function init() {
             if (!configService.getLogin()) return $state.go('login');
-            fileInput.addEventListener('change', readFile);
-
+            
         }
 
-        function readFile() {
+        function processFile() {
+            vm.orders = [];
+
+            readFile(function (result) {
+                var list = [];
+                var total_lines = result.length;
+                var csvWorker = new Worker('/js/worker.js');
+                var count = 0;
+                csvWorker.addEventListener('message', function (message) {
+                    list.push(message.data);
+                    count++;
+                    if (count >= total_lines) csvWorker.terminate();
+                });
+
+                for (var i = 0; i < total_lines; i++) {
+                    csvWorker.postMessage(result[i]);
+                }
+                
+            });
+        }
+
+        function readFile(callback) {
 
             var reader = new FileReader();
-
             reader.onload = function () {
-                document.getElementById('content').innerHTML = reader.result;
+               return callback(reader.result.split('\r\n'));
             }
-
             reader.readAsBinaryString(fileInput.files[0]);
             
         }
+
+       
 
     }
 })();
---
+++ Cibertec.Angular/wwwroot/js/worker.js
@@ -0,0 +1,12 @@
+﻿var self = this;
+
+self.onmessage = function (message) {
+    
+    var elements = message.data.split(',');
+    var order = {
+        id: elements[0], orderid: elements[1], productid: elements[2],
+        unitprice: elements[3], quantity: elements[4]
+    };
+    self.postMessage(order);
+}
+
